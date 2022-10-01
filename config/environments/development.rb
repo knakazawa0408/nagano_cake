62fd529ad273cf2d6c77f5ac43f279a79f4bc900
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-10-01 03:45:45 +0000

    環境作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "79091a888d8a4049b643fd34ed1e3bb5.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
