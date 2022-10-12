d22ec917f28d9b6dae3364b522edc563ce04c0e3
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-10-12 16:57:03 +0000

    [ADD]管理者ユーザー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(email: 'naganocake@gmail.com', password: 'aaaaaa', confirmed_at: Time.now)
